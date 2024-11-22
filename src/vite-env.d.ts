6a66edf4fab470bc79c982d798bba114baca3ad7
fernando <faguiar408@gmail.com>
2024-11-22 20:06:49 -0300

    feat: add environment vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    readonly VITE_API_HOST: string;
+    readonly VITE_API_URL: string;
+    readonly VITE_API_WS: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
